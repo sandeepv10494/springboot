8d29e606558f6b08a88c0688c27d2039415c1cb7
Sandeep.V <Sandeep.V@YCSLAP0268.yethi.in>
2019-12-25 17:18:52 +0530

    Updated packaging to war

--- cruddemo/src/main/java/com/sandeep/cruddemo/CruddemoApplication.java
+++ cruddemo/src/main/java/com/sandeep/cruddemo/CruddemoApplication.java
@@ -2,10 +2,17 @@ package com.sandeep.cruddemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CruddemoApplication {
+public class CruddemoApplication extends SpringBootServletInitializer{
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(CruddemoApplication.class);
+    }
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CruddemoApplication.class, args);
 	}
